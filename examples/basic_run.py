a0f32ea3c8fc568857e0ec88dc5304d1134084cf
Tadej Lahovnik <tadej.lahovnik@student.um.si>
2024-08-01 13:28:03 +0200

    fix: Dynamic paths Dynamic paths to data files, disclaimer in basic run example

--- examples/basic_run.py
+++ examples/basic_run.py
@@ -1,11 +1,20 @@
+"""
+Basic run example showcasing tinynarm usage.
+
+Before running this example, run the discretization example.
+"""
+import os
+import time
 from tinynarm import TinyNarm
 from tinynarm.utils import Utils
-import time
 
 # start timer -> for experimental purposes
 start = time.time()
 
-tnarm = TinyNarm("sportydatagen_discretized.csv")
+input_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "sportydatagen_discretized.csv")
+output_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "rules.csv")
+
+tnarm = TinyNarm(input_path)
 tnarm.create_rules()
 
 print("Program execution --- %s seconds ---" % (time.time() - start))
@@ -13,6 +22,6 @@ print("Program execution --- %s seconds ---" % (time.time() - start))
 postprocess = Utils(tnarm.rules)
 postprocess.add_fitness()
 postprocess.sort_rules()
-postprocess.rules_to_csv("rules.csv")
+postprocess.rules_to_csv(output_path)
 postprocess.generate_statistics()
 postprocess.generate_stats_report(20)
